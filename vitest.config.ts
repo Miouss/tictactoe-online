e8bbd3f69955601d7992f560128995bdbbcb5c99
Miouss <samir.ghabi@gmail.com>
2023-03-13 13:01:22 +0100

    Update test config

---
+++ vitest.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from "vite";
+import path from "path";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  resolve: {
+    alias: {
+      "@database": path.resolve(__dirname, "./packages/Node/src/database"),
+      "@server": path.resolve(__dirname, "./packages/Node/src/server"),
+      "@handlers": path.resolve(__dirname, "./packages/Node/src/handlers"),
+      "@utils": path.resolve(__dirname, "./packages/Node/src/utils"),
+      "@types": path.resolve(__dirname, "./packages/Node/src/types"),
+      "@middlewares": path.resolve(__dirname, "./packages/Node/src/middlewares"),
+      "@controllers": path.resolve(__dirname, "./packages/Node/src/controllers"),
+    },
+  },
+});
