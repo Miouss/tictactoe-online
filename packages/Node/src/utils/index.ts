ea4170dbfdda3ed52c2f800510c975924c12304a
Miouss <samir.ghabi@gmail.com>
2023-03-14 22:28:29 +0100

    Relocated some tests' utils

--- packages/Node/src/utils/index.ts
+++ packages/Node/src/utils/index.ts
@@ -5,4 +5,8 @@ export * from "./common/createPlayer";
 export * from "./common/wait";
 
 // tests
-export * from "./tests/lobby";
+export * from "./tests/common";
+export * from "./tests/mock";
+
+// lobby
+export * from "./lobby/removePlayerFromLobby";
---
+++ packages/Node/src/utils/tests/mock.ts
@@ -0,0 +1,19 @@
+import { Player } from "@types";
+import { createPlayer } from "@utils";
+
+export function mockPlayers(...names: string[]): Player[] {
+  return names.map((name, index) => createPlayer(name, `${index}`));
+}
+
+export function mockLobby(...players: Player[]) {
+  return {
+    id: "123",
+    players,
+    save() {
+      return Promise.resolve;
+    },
+    delete() {
+      return Promise.resolve;
+    },
+  };
+}
