e572f1e21ed3d45dcd4f00ca528ce29ea17d47f4
Miouss <samir.ghabi@gmail.com>
2023-03-19 22:32:25 +0100

    Rename main handlers' file

--- packages/Node/src/handlers/index.ts
+++ packages/Node/src/handlers/index.ts
@@ -1,5 +1,5 @@
-export * from "./game/game";
-export * from "./lobby/lobby";
+export * from "./game";
+export * from "./lobby";
 export * from "./lobby/createLobby";
 export * from "./lobby/joinLobby";
 export * from "./lobby/leaveLobby";
